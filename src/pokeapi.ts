4a32f46aa84771878ab760791db241afe36a9ad8
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-17 13:45:04 -0700

    add location type

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -28,11 +28,61 @@ export type ShallowLocations = {
   count: number;
   next: string | null;
   previous: string | null;
-  results: Location[];
+  results: {
+    name: string;
+    url: string;
+  }[];
 };
 
 export type Location = {
-  // add properties here
+  encounter_method_rates: {
+    encounter_method: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      rate: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
+  game_index: number;
+  id: number;
+  location: {
+    name: string;
+    url: string;
+  };
   name: string;
-  url: string;
+  names: {
+    language: {
+      name: string;
+      url: string;
+    };
+    name: string;
+  }[];
+  pokemon_encounters: {
+    pokemon: {
+      name: string;
+      url: string;
+    };
+    version_details: {
+      encounter_details: {
+        chance: number;
+        condition_values: any[];
+        max_level: number;
+        method: {
+          name: string;
+          url: string;
+        };
+        min_level: number;
+      }[];
+      max_chance: number;
+      version: {
+        name: string;
+        url: string;
+      };
+    }[];
+  }[];
 };
