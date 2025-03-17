be918f3904ea1851a4d48469256ec07fd7d9272b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-03-17 11:18:47 -0700

    create startREPL func

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { cleanInput, startREPL } from "./repl.js";
+
 function main() {
-  console.log("Hello, world!");
+  //   console.log("Hello, world!");
+  startREPL();
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,29 @@
+import { createInterface } from "node:readline";
+import { stdin, stdout } from "node:process";
 export function cleanInput(input: string): string[] {
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter((val) => val !== "");
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.on("line", (input) => {
+    const arrResults = cleanInput(input);
+    if (arrResults.length == 0) {
+      rl.prompt();
+      return;
+    }
+    stdout.write(`Your command was: ${arrResults[0]}\n`);
+    rl.prompt();
+  });
+
+  rl.prompt();
 }
